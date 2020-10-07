cd6c13d49353a6d33e42ff0ef39ce59714c19cf7
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2020-10-06 20:38:06 -0600

    Update files

--- src/main/java/com/controllers/PersonController.java
+++ src/main/java/com/controllers/PersonController.java
@@ -5,9 +5,12 @@
  */
 package com.controllers;
 
+import com.google.gson.Gson;
 import com.models.Person;
 import java.util.ArrayList;
 import com.services.PersonService;
+import spark.Request;
+import spark.Response;
 /**
  *
  * @author jzuniga
@@ -19,21 +22,30 @@ public class PersonController {
         this.personService = new PersonService();
     }
     
-    
-    
-    public ArrayList<Person> index() {
+    public ArrayList<Person> index(Request req, Response res) {
         return this.personService.getAll();
     }
     
-    public Person show(int id) {
+    public Person showByEmail(Request req, Response res) {
+        if(req.queryParams("email") != null) {   
+            String email = req.queryParams("email");
+            return this.personService.getByEmail(email);
+        } else
+            return null;
+    }
+    
+    public Person show(Request req, Response res) {
+         int id = Integer.parseInt(req.params(":id"));
         return this.personService.get(id);
     }
     
-    public Person showByEmail(String email) {
-        return this.personService.getByEmail(email);
-    }
-    
-    public void store(Person person) {
-        this.personService.save(person);
+    public String store(Request req, Response res) {
+         try {
+            Person person = new Gson().fromJson(req.body(), Person.class);
+            this.personService.save(person);
+            return "Success";
+        } catch(Exception e) {
+            return e.getMessage();
+        }
     }
 }
